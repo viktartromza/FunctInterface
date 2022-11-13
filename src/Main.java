b7396597f606ebb07efa6632ead12b0c9a6b4f6f
ViktarTromza <viktar.tromza@gmail.com>
2022-11-13 15:43:22 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.function.*;
-
 
 public class Main {
     public static void main(String[] args) {
